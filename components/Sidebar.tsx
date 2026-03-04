b2744090527321969c2ea63bfae13913a779e888
AF <af.arthurfaria@gmail.com>
2026-03-04 20:40:02 -0300

    feat: add Sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+interface NavItem {
+  label: string;
+  icon: string;
+  href: string;
+}
+
+const navItems: NavItem[] = [
+  { label: "Home", icon: "/Home.svg", href: "/" },
+  { label: "Posts", icon: "/Grid.svg", href: "https://afaaafa.substack.com/" },
+  { label: "Contact", icon: "/Direct.svg", href: "/contact" },
+];
+
+export function Sidebar() {
+  const [hoveredItem, setHoveredItem] = useState<string | null>(null);
+  const [hoveredSidebar, setHoveredSidebar] = useState(false);
+
+  return (
+    <aside className={`fixed left-0 top-0 h-screen border-white/10 flex flex-col items-center justify-center gap-4 z-40 px-4 py-4 ${hoveredSidebar ? "w-48" : "w-20"} transition-width duration-300`}       onMouseEnter={() => setHoveredSidebar(true)} 
+    onMouseLeave={() => setHoveredSidebar(false)}>
+      {navItems.map((item) => (
+        <Link
+          key={item.label}
+          href={item.href}
+          target="_blank"
+          className="w-full"
+        >
+          <div
+            className={`relative flex justify-left px-4 py-4 rounded-lg ${hoveredItem === item.label ? "bg-[#282a2c]" : ""}` }
+            onMouseEnter={() => setHoveredItem(item.label)}
+            onMouseLeave={() => setHoveredItem(null)}
+          >
+            <div className="shrink-0">
+              <Image
+                src={item.icon}
+                alt={item.label}
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+            </div>
+
+            {hoveredSidebar && (
+              <span className="mx-4 text-white">{item.label}</span>
+            )}
+          </div>
+        </Link>
+      ))}
+    </aside>
+  );
+}
